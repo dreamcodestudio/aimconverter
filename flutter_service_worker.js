24625feaf4daec35d8785d6f5c7bbaf4e431fd0c
yapetrichka <yapetrichka@playflock.com>
2022-04-16 15:31:11 +0300

    Upd scroll physics

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "a906c14ac4db3229dea6391350481b5d",
 "icons/Icon-maskable-192.png": "fd2a066bd87132b0e3f9bdc49c4729f9",
 "icons/Icon-maskable-512.png": "a906c14ac4db3229dea6391350481b5d",
-"index.html": "d0616ebfe990831cc3a453c89c7b9c0d",
-"/": "d0616ebfe990831cc3a453c89c7b9c0d",
-"main.dart.js": "c72489481adfacdf285794beaf6c75f3",
+"index.html": "8594b976dc535a68b836d79619c4a755",
+"/": "8594b976dc535a68b836d79619c4a755",
+"main.dart.js": "e19d9adf71df3e8fa2d557cb2e182ddc",
 "manifest.json": "4d1e80598511c566cbc3e56f580452b0",
 "version.json": "0f8aaf9a32bdaee4704f4b57df3e8c2b"
 };
